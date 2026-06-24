72e59c8aa4d7612f1d92cf24dd0a07f5b468d967
Laura Dean <lldean18@gmail.com>
2026-06-24 09:50:13 +0100

    this failed but with no error message so trying again. If it won't run still I'll try with the remote option as that was defo working before

--- 07_checkout_unmapped_reads.bash
+++ 07_checkout_unmapped_reads.bash
@@ -8,8 +8,8 @@
 #SBATCH --partition=defq
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=10g
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=15g
 #SBATCH --time=40:00:00
 #SBATCH --output=slurm-%x-%j.out
 
@@ -20,33 +20,35 @@ source $HOME/.bash_profile
 cd /gpfs01/home/mbzlld/data/tradis/biotradis10
 
 
-# extract unmapped reads as fasta
-conda activate samtools1.22
-samtools fastq -f 4 BWtacXpress_merge123_1.fq.gz.mapped.bam > unmapped.fastq
-conda deactivate
-conda activate seqkit
-seqkit fq2fa unmapped.fastq > unmapped.fasta
-conda deactivate
-rm unmapped.fastq
-
-
-# extract unmapped reads >30bp
-conda activate seqkit
-seqkit seq -m 30 unmapped.fasta > unmapped_30bp_plus.fasta
-conda deactivate
+##  # extract unmapped reads as fasta
+##  conda activate samtools1.22
+##  samtools fastq -f 4 BWtacXpress_merge123_1.fq.gz.mapped.bam > unmapped.fastq
+##  conda deactivate
+##  conda activate seqkit
+##  seqkit fq2fa unmapped.fastq > unmapped.fasta
+##  conda deactivate
+##  rm unmapped.fastq
+##  
+##  
+##  # extract unmapped reads >30bp
+##  conda activate seqkit
+##  seqkit seq -m 30 unmapped.fasta > unmapped_30bp_plus.fasta
+##  conda deactivate
 
 
 # blast those reads (against my locally downloaded copy of the database)
 conda activate blast
 blastn \
-  -query unmapped_30bp_plus.fasta \
-  -db /gpfs01/home/mbzlld/data/databases/nt \
-  -max_target_seqs 1 \
-  -max_hsps 1 \
-  -outfmt "6 qseqid sseqid stitle pident length evalue bitscore" \
-  -out top_hits.tsv
+-query unmapped_30bp_plus.fasta \
+-db /gpfs01/home/mbzlld/data/databases/nt \
+-max_target_seqs 1 \
+-max_hsps 1 \
+-outfmt "6 qseqid sseqid stitle pident length evalue bitscore" \
+-out top_hits.tsv
+
 conda deactivate
 
+echo "blast finished running"
 
 ##  # blast those reads (against the server database)
 ##  conda activate blast
